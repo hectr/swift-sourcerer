26645455fefad04eb6a5e1400f0eeabcae89bbbf
hectr <h@mrhector.me[core]>
2018-11-24 00:40:15 +0100

    Separate public/internal and classes/structs in untested.md

--- Sources/sourcerer/renderers/UntestedRenderer.swift
+++ Sources/sourcerer/renderers/UntestedRenderer.swift
@@ -13,14 +13,50 @@ public final class UntestedRenderer: AbstractRenderer<TypesReport> {
     }
 
     public override func render() {
-        writer.writeTitle("Untested classes and structs")
+        writer.writeTitle("Untested public types")
+        writer.writeTitle("Classes", level: 1)
         writer.lines += [""]
-        writer.lines += untestedClassesAndStructsNames.map { "- \($0)" }
+        writer.lines += untestedPublicClasses.map { "- \($0)" }
+        writer.writeTitle("Structs", level: 1)
+        writer.lines += [""]
+        writer.lines += untestedPublicStructs.map { "- \($0)" }
+        writer.writeTitle("Untested internal types")
+        writer.writeTitle("Classes", level: 1)
+        writer.lines += [""]
+        writer.lines += untestedInternalClasses.map { "- \($0)" }
+        writer.writeTitle("Structs", level: 1)
+        writer.lines += [""]
+        writer.lines += untestedInternalStructs.map { "- \($0)" }
     }
 
-    private var untestedClassesAndStructsNames: [String] {
-        let publicClasses = classesAndStructs.filter { $0.hasPublicAccess && $0.hasPublicAccessMethods }
-        let typeNames = publicClasses.map { $0.name.replacingOccurrences(of: ".", with: "") }
+    private var untestedPublicClasses: [String] {
+        let publicClasses = classes
+            .filter { $0.hasPublicAccess && $0.hasPublicAccessMethods }
+        return untestedTypes(types: publicClasses)
+    }
+
+    private var untestedPublicStructs: [String] {
+        let publicClasses = structs
+            .filter { $0.hasPublicAccess && $0.hasPublicAccessMethods }
+        return untestedTypes(types: publicClasses)
+    }
+
+    private var untestedInternalClasses: [String] {
+        let publicClasses = classes
+            .filter { $0.hasInternalAccess && ($0.hasInternalAccessMethods || $0.hasPublicAccessMethods) }
+        return untestedTypes(types: publicClasses)
+    }
+
+    private var untestedInternalStructs: [String] {
+        let publicClasses = structs
+            .filter { $0.hasInternalAccess && ($0.hasInternalAccessMethods || $0.hasPublicAccessMethods) }
+        return untestedTypes(types: publicClasses)
+    }
+
+    private func untestedTypes(types: [Type]) -> [String] {
+        let typeNames = types
+            .map { $0.name.replacingOccurrences(of: ".", with: "") }
+            .sorted { $0.count > $1.count }
         var untestedTypes = Set(typeNames)
         var testCasesNames = Set(testCases.map { $0.localName })
         for typeName in typeNames {
@@ -56,6 +92,28 @@ private extension Type {
             switch accessLevel {
             case .none, .internal, .fileprivate, .private: continue
             case .public, .open: return true
+            }
+        }
+        return false
+    }
+
+    var hasInternalAccess: Bool {
+        guard let accessLevel = AccessLevel(rawValue: self.accessLevel) else { return false }
+        switch accessLevel {
+        case .none, .internal: return true
+        case .public, .open: return false
+        case .fileprivate, .private: return false
+        }
+    }
+
+    var hasInternalAccessMethods: Bool {
+        let methods = self.methods.filter { !$0.isInitializer }
+        for method in methods {
+            guard let accessLevel = AccessLevel(rawValue: method.accessLevel) else { return false }
+            switch accessLevel {
+            case .none, .internal: return true
+            case .public, .open: continue
+            case .fileprivate, .private: continue
             }
         }
         return false
