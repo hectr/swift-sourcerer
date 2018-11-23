c36ecb96474dafa1e58494f71c1f4ac4074136ef
hectr <h@mrhector.me[core]>
2018-11-24 00:38:51 +0100

    Add prefixes and more suffixes to isTestCase

--- Sources/SourcererTypes/Class+Utils.swift
+++ Sources/SourcererTypes/Class+Utils.swift
@@ -3,10 +3,16 @@ import SourceryRuntime
 
 public extension Class {
     var isTestCase: Bool {
-        let suffixes = ["Spec", "Tests", "Test", "TestCase", "Should"]
+        let suffixes = ["Specs", "Spec", "Tests", "Test", "TestCases", "TestCase", "Should"]
+        let prefixes = ["ST_"]
         for inheritedType in inheritedTypes + [name] {
             for suffix in suffixes {
                 if inheritedType.hasSuffix(suffix) {
+                    return true
+                }
+            }
+            for prefix in prefixes {
+                if inheritedType.hasPrefix(prefix) {
                     return true
                 }
             }
